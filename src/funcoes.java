4f96c3fc941a75c398814a96d8b55995fd044d20
Giovana <giovanadossantos107@gmail.com>
2025-09-13 17:48:43 -0300

    função para saudação, soma e resultado fora do main

--- src/funcoes.java
+++ src/funcoes.java
@@ -1,5 +1,22 @@
 public class funcoes {
     public static void main(String[] args) {
+        String nomeOriginal = "Lucas";
+        saudacao(nomeOriginal);
 
+        //criando o resultado e os valores
+        int resultado = soma (2, 3);
+
+        //Imprimindo o valor do resultado da soma
+        System.out.println(resultado);
     }
+     public static void saudacao (String nomeParametro) {
+         //Função para saudação
+
+         System.out.println("Olá" + nomeParametro);
+     }
+
+     //retorna o valor de a + b
+     public static int soma (int a, int b) {
+        return a + b;
+     }
 }
